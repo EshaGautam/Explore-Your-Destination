d5f815b7660c51336caa4f0344a16e639dead122
Esha Gautam <eshagautam0@gmail.com>
2026-08-14 12:24:42 +0530

    setup datebase connection

---
+++ server/config/database.js
@@ -0,0 +1,31 @@
+import neo4j from "neo4j-driver";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const requiredEnv = [
+  "COGNODB_URI",
+  "COGNODB_USERNAME",
+  "COGNODB_PASSWORD",
+];
+
+for (const key of requiredEnv) {
+  if (!process.env[key]) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+}
+
+const driver = neo4j.driver(
+  process.env.COGNODB_URI,
+  neo4j.auth.basic(
+    process.env.COGNODB_USERNAME,
+    process.env.COGNODB_PASSWORD
+  )
+);
+
+export const verifyDatabaseConnection = async () => {
+  await driver.verifyConnectivity();
+  console.log("Connected to CognoDB successfully");
+};
+
+export default driver;
